ba7a1fb9d85de9e6857519e15e584448d57fa9a3
Wayne Montague <wayne.montague@gmail.com>
2013-09-20 16:06:22 -0700

    _.each() has been implemented

--- src/underbar.js
+++ src/underbar.js
@@ -38,6 +38,16 @@ var _ = { };
   // Call iterator(value, key, collection) for each element of collection.
   // Accepts both arrays and objects.
   _.each = function(collection, iterator) {
+    var key;
+    if (Array.isArray(collection)) {
+      for (var i = 0; i < collection.length; i++) {
+        iterator(collection[i], i, collection);
+      }
+    } else if (typeof collection === 'object') {
+      for (key in collection) {
+        iterator(collection[key], key, collection);
+      }
+    }
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
